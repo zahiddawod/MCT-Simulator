76bf87d766f9688c80dc63940a62a4fc374f9aca
Zahid <zahiddawod@hotmail.com>
2020-03-24 21:11:15 -0400

    Fixed title not updating

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,10 +2,9 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-    this->setFixedSize(400, 800); // Set size of the window;
-    this->setWindowTitle(QString("DENAS Simulator"));
     ui->setupUi(this);
-
+    this->setWindowTitle("DENAS Simulator");
+    this->setFixedSize(400, 800); // Set size of the window;
 }
 
 // Deconstructor not required with the parenting system, when the Window instance is out of the stack, the objects will automatically be deleted from the heap.
